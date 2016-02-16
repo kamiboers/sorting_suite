30824aa1dfe4f4b86aecc23e240c9cfd783e42f2
Kami Boers <kamiboers@gmail.com>
2016-02-16 00:14:01 -0700

    insertion finals

--- lib/insertion_sort.rb
+++ lib/insertion_sort.rb
@@ -6,27 +6,39 @@ class InsertionSort
   end
 
   def sort(array)
-    print_unsorted_array(array)
+    if array.length == 1
+      puts "Sorted Array: #{array}"
+    elsif array.length == 0
+      puts "Empty Array"
+    else
+      continue_sort(array)
+    end
+  array
+  end
+
+  def continue_sort(array)
+    puts display_unsorted_array(array)
     timer_start
-    traverse_and_insert_value_where_greater_than_previous_value(array)
+    insert_value_where_greater_than_previous_value(array)
     timer_stop
     return_sorted_array(array)
   end
 
-  def traverse_and_insert_value_where_greater_than_previous_value(array)
-    array.each_with_index do |value, position| #goes through the array from 0 to final position, taking note of value and position in the array
+
+  def insert_value_where_greater_than_previous_value(array)
+    array.each_with_index do |value, position| #traverses forward, holding value and position, which move into the 'sorted' side of the array
       traverse_left = position - 1 #initially gives the value of the position to the left of the value/position in focus
       while traverse_left >= 0 #ensures that nil doesn't make an appearance by only attempting to compare values if a value exists in the array
         break if array[traverse_left] <= value #exits leftwards traversal if the value to the left is less than the value in focus
-        array[traverse_left+1] = array[traverse_left] #places the greater value of the position to the left in the position to its right, this continues until reaching zero
+        array[traverse_left+1] = array[traverse_left] #places the greater value of the position to the left in the position to its right, this continues until reaching position zero
         traverse_left -= 1 #traverses left one place
       end
       array[traverse_left+1] = value #controls for first position's participation, returning array[0] = current value, and places the hanging value of the original position into the array appropriately
     end
   end
 
-  def print_unsorted_array(array)
-    puts "Unsorted array: #{array.to_s}"
+  def display_unsorted_array(array)
+    "Unsorted array: #{array.to_s}"
   end
 
   def timer_start
@@ -46,5 +58,5 @@ end
 
 if __FILE__ == $0
   test = InsertionSort.new
-  test.sort((1..100).to_a.shuffle)
+  test.sort((1..1000).to_a.shuffle)
 end
--- lib/insertion_sort_test.rb
+++ lib/insertion_sort_test.rb
@@ -2,87 +2,41 @@ require 'pry'
 gem 'minitest', '~> 5.2'
 require 'minitest/autorun'
 require 'minitest/pride'
-require_relative 'bubblier'
+require_relative 'insertion_sort'
 
-class BubbleTest < Minitest::Test
-
-  def test_initial_time_is_nil
-    test = BubbleSort.new
-    assert @time_start==nil
-  end
-
-  def test_timer_records_static_starting_time
-    test = BubbleSort.new
-    new_time = test.timer_start
-    assert new_time != nil
-    assert new_time.class == Time
-    assert new_time != Time.now
-  end
-
-  def test_prints_original_scrambled_array
-    test = BubbleSort.new
-    assert_equal "\nScrambled values:\n [4, 1, 2, 3]", test.display_original([4,1,2,3])
-  end
-
-  def test_recognizes_neighbors_out_of_order
-    test = BubbleSort.new
-    assert test.neighbors_out_of_order?([2,1])
-    refute test.neighbors_out_of_order?([1,2])
-  end
-
-  def test_swap_two_positions_on_demand
-    test = BubbleSort.new
-    assert_equal [1,2], test.switch_neighboring_value_order([2,1])
-    assert_equal [2,1], test.switch_neighboring_value_order([1,2])
-  end
-
-  def test_swap_only_when_out_of_order
-    test = BubbleSort.new
-    assert_equal [1,2], test.compare_neighboring_values([2,1], 0)
-    assert_equal [1,2], test.compare_neighboring_values([1,2], 0)
-  end
+class InsertionTest < Minitest::Test
 
   def test_sorts_an_unsorted_array
-    test = BubbleSort.new
+    test = InsertionSort.new
     array = [5, 6, 9, 2, 8, 1, 7, 10, 3, 4]
     assert_equal array.sort, test.sort(array)
   end
 
   def test_does_not_affect_a_sorted_array
-    test = BubbleSort.new
+    test = InsertionSort.new
     array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     assert_equal array, test.sort(array)
   end
 
   def test_operates_on_letters_as_strings
-    test = BubbleSort.new
+    test = InsertionSort.new
     array = ["d", "c", "b", "a"]
     assert_equal ["a", "b", "c", "d"], test.sort(array)
   end
 
-  def value_switch_records_switch_invalidates_sorted_traverse
-    test = BubbleSort.new
-    @sorted = true
-    @switches = 0
-    test.one_value_switch_occurs
-    assert_equal false, @sorted
-    assert_equal 1, @switches
+  def test_ignores_and_returns_empty_or_single_value_arrays
+    test = InsertionSort.new
+    array = []
+    assert_equal [], test.sort(array)
+    array = [17000]
+    assert_equal [17000], test.sort(array)
   end
 
-  def traverse_marks_final_value_sorted
-    test = BubbleSort.new
-    @num_fewer_comparisons = 0
-    array = [5,4,3,2,1]
-    test.traverse(array)
-    assert_equal 1, @num_fewer_comparisons
+  def test_reversed_array_returned_sorted
+    test = InsertionSort.new
+    array = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+    assert_equal array.sort, test.sort(array)
   end
 
-
-  def timer_stop_returns_elapsed_time
-    test = BubbleSort.new
-    test.timer_start
-    time_elapsed = test.timer_stop
-    assert_equal Time, time_elapsed.class
-  end
 
 end
